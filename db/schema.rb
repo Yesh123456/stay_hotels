8f99c91de1e22129a2f443708b8db9d88da5f43f
Yesh123456 <yeshjadav4@gmail.com>
2025-02-06 23:08:09 +0530

    08 - Show Property page with frontend

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_28_184224) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_06_153944) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "properties", force: :cascade do |t|
+    t.string "name"
+    t.string "long_title"
+    t.text "description"
+    t.string "address_line_1"
+    t.string "address_line_2"
+    t.string "city"
+    t.string "state"
+    t.string "country"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,12 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Property.find_or_create_by!(name: 'Taj Hotels',
+	long_title: 'Taj Hotels is the best',
+	description: 'Taj Hotels is the best',
+	address_line_1: 'Florida Road',
+	address_line_2: 'New Jersey Lane',
+	city: 'New York',
+	state: 'New York',
+	country: 'United States')
